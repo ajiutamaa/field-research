52fa8f1c712619216fa6361930a629f4106e0b70
ajiutamaa <prasetya.ajie@ui.ac.id>
2015-11-13 15:43:49 +0700

    cors

--- app/Global.java
+++ app/Global.java
@@ -4,6 +4,12 @@ import play.Application;
 import play.GlobalSettings;
 import play.Logger;
 
+import play.libs.F;
+import play.mvc.Action;
+import play.mvc.Http;
+import play.mvc.Result;
+
+import java.lang.reflect.Method;
 /**
  * Created by lenovo on 11/5/2015.
  */
@@ -19,5 +25,26 @@ public class Global extends GlobalSettings {
             Logger.error(e.getMessage());
         }
         Logger.debug("--- app ready ---");
+    }
+
+    @Override
+    public Action onRequest(Http.Request request, Method method) {
+        return new ActionWrapper(super.onRequest(request, method));
+    }
+
+    /** for CORS */
+    private class ActionWrapper extends Action.Simple {
+
+        public ActionWrapper(Action<?> action) {
+            this.delegate = action;
+        }
+
+        @Override
+        public F.Promise<Result> call(Http.Context context) throws Throwable {
+            F.Promise<Result> result = this.delegate.call(context);
+            Http.Response response = context.response();
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            return result;
+        }
     }
 }
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,4 +7,12 @@ public class Application extends Controller {
     public static Result hello() {
         return ok(toJson("hello"));
     }
+
+    public static Result preflight(String all) {
+        response().setHeader("Access-Control-Allow-Origin", "*");
+        response().setHeader("Allow", "*");
+        response().setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+        response().setHeader("Access-Control-Allow-Headers", "Origin, X-AUTH-TOKEN, X-Requested-With, Content-Type, Accept, Referer, User-Agent");
+        return ok();
+    }
 }
